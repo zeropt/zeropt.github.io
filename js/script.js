eafc4b39aec508d082034d3edabba4c4d1583fa8
zeropt <ferr0fluidmann@gmail.com>
2025-04-22 08:39:31 -0700

    rough functionality and theme

--- js/script.js
+++ js/script.js
@@ -2,17 +2,140 @@
  * script.js
  * Author: Riley Mann
  * 
+ * Main script for zeropt.github.io
+ * 
  * Created on 16 Apr. 2025
  */
 
-let audio = $("audio")[0];
+// Constants
+const BGWIDTH = 512;
+const BGHEIGHT = 216;
+const CACTUSWIDTH = 64;
 
-$(document).ready(function(){
-	$("p").click(function(){
-		audio.play();
-	});
+// Default settings
+const settings = {
+	"theme": "light",
+	"muted": false
+};
 
-	$.get("test.md", function(data){
-		$("#content").html(marked.parse(data));
-	});
-});
+/*-----------------------------------Setup------------------------------------*/
+
+$(document).ready(setup);
+
+function setup() {
+	// Local storage
+	if (localStorage.theme) settings.theme = localStorage.theme;
+	else localStorage.theme = settings.theme;
+	// Theme
+	if (settings.theme == "dark") dusk();
+	// Initial rescale
+	rescale();
+	$(window).resize(rescale);
+	// Connect buttons
+	$("#theme-btn").click(toggleTheme);
+	$("#mute-btn").click(toggleMute);
+	$("#aboutme-btn").click(function(){changeSubpage("#aboutme");});
+	$("#projects-btn").click(function(){changeSubpage("#projects");});
+	$("#links-btn").click(function(){changeSubpage("#links");});
+	$("#contact-btn").click(function(){changeSubpage("#contact");});
+	// Load subpage
+	loadSubpage(location.hash);
+	window.onhashchange = function(){loadSubpage(location.hash);};
+}
+
+/*------------------------------Layout Functions------------------------------*/
+
+function rescale() {
+	let width = $(window).width();
+	let height = $(window).height();
+	// Calculate pixel width
+	let pixelWidth = 0.0;
+	if (width * BGHEIGHT / height > BGWIDTH)
+		pixelWidth = width / BGWIDTH; // window is wide
+	else pixelWidth = height / BGHEIGHT; // window is tall
+	// Scale cactus
+	let newCactusWidth = Math.round(CACTUSWIDTH * pixelWidth);
+	$("#cactus-img").width(newCactusWidth);
+}
+
+/*-----------------------------Settings Functions-----------------------------*/
+
+function toggleTheme() {
+	// Toggle setting
+	if (settings.theme == "dark") {
+		settings.theme = "light";
+		dawn();
+	} else {
+		settings.theme = "dark";
+		dusk();
+	}
+	// Set local storage
+	localStorage.theme = settings.theme;
+}
+
+function dawn() {
+	$("#theme-btn").removeClass("dark");
+}
+
+function dusk() {
+	$("#theme-btn").addClass("dark");
+}
+
+function toggleMute() {
+	if (settings.muted) {
+		settings.muted = false;
+		unmute();
+	} else {
+		settings.muted = true;
+		mute();
+	}
+}
+
+function mute() {
+	$("#mute-btn").addClass("muted");
+}
+
+function unmute() {
+	$("#mute-btn").removeClass("muted");
+}
+
+/*--------------------------------Menu Handler--------------------------------*/
+
+function changeSubpage(locHash) {
+	playSound();
+	location.hash = locHash;
+}
+
+function loadSubpage(locHash) {
+	// Clear content box
+	$("#content-box").empty();
+	$("#content-box").show();
+	// Load content
+	// About me page
+	if (locHash == "#aboutme") {
+		$("#content-box").load("pages/aboutme.html");
+	// Projects page
+	} else if (locHash == "#projects") {
+		$.get("projects/test/test.md", function(data){
+			$("#content-box").html(marked.parse(data));
+		});
+	// Links page
+	} else if (locHash == "#links") {
+		$("#content-box").load("pages/links.html");
+	// Contact page
+	} else if (locHash == "#contact") {
+		$("#content-box").load("pages/contact.html");
+	} else {
+		$("#content-box").hide();
+	}
+}
+
+/*--------------------------------More Actions--------------------------------*/
+
+function playSound() {
+	if (!settings.muted) {
+		const sound = $("#pluck-sound")[0];
+		sound.load();
+		sound.play();
+	}
+}
---
+++ projects/test/test.md
@@ -0,0 +1,25 @@
+# This is a heading
+
+![alt text](projects/test/img/hollow.jpg)
+
+**Filler:** *Lorem ipsum* dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## 2 Heading 2 Furious
+
+1. one
+2. two
+3. three
+4. four
+
+- these
+- are
+- list
+- elements
+
+```py
+import math
+
+x = 2
+
+print(x**2)
+```
