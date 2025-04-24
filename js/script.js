b8c67b73568e0a7292c1112b701add8bf8bed4ea
zeropt <ferr0fluidmann@gmail.com>
2025-04-23 19:01:15 -0700

    implement project menu

--- js/script.js
+++ js/script.js
@@ -13,36 +13,47 @@ const settings = {
 	"muted": false
 };
 
+// Project paths
+const projectDir = "projects/";
+const projectListPath = "projects/projectlist.json";
+
+const projectIDs = [];
+
 /*-----------------------------------Setup------------------------------------*/
 
 $(document).ready(setup);
 
 function setup() {
-	// Local storage
-	if (localStorage.theme) settings.theme = localStorage.theme;
-	else localStorage.theme = settings.theme;
-	// Theme
-	if (settings.theme == "dark") dusk();
-	// Connect buttons
-	$("#theme-btn").click(toggleTheme);
-	$("#mute-btn").click(toggleMute);
-	// Attach Sounds
-	$(".click1").mouseover(function(){playSound("#click1-hover");});
-	$(".click1").mousedown(function(){playSound("#click1-press");});
-	$(".click1").mouseup(function(){playSound("#click1-release");});
-	$(".click2").mouseover(function(){playSound("#click1-hover");});
-	$(".click2").mousedown(function(){playSound("#click2-press");});
-	$(".click2").mouseup(function(){playSound("#click2-release");});
-	$(".click3").mouseover(function(){playSound("#click1-hover");});
-	$(".click3").mousedown(function(){playSound("#click3-press");});
-	$(".click3").mouseup(function(){playSound("#click3-release");});
-	$("#mute-btn").mouseover(function(){playSound("#click1-hover");});
-	// Load subpage
-	loadSubpage(location.hash);
-	window.onhashchange = function(){loadSubpage(location.hash);};
-	// Cactus
-	positionCactus();
-	$(window).resize(positionCactus);
+	// Build project menu before anything else
+	buildProjectMenu(function(){
+		// Local storage
+		if (localStorage.theme) settings.theme = localStorage.theme;
+		else localStorage.theme = settings.theme;
+		// Theme
+		if (settings.theme == "dark") dusk();
+		// Connect buttons
+		$("#theme-btn").click(toggleTheme);
+		$("#mute-btn").click(toggleMute);
+		// Attach Sounds
+		$(".click1").mouseover(function(){playSound("#click1-hover");});
+		$(".click1").mousedown(function(){playSound("#click1-press");});
+		$(".click1").mouseup(function(){playSound("#click1-release");});
+		$(".click2").mouseover(function(){playSound("#click1-hover");});
+		$(".click2").mousedown(function(){playSound("#click2-press");});
+		$(".click2").mouseup(function(){playSound("#click2-release");});
+		$(".click3").mouseover(function(){playSound("#click1-hover");});
+		$(".click3").mousedown(function(){playSound("#click3-press");});
+		$(".click3").mouseup(function(){playSound("#click3-release");});
+		$("#mute-btn").mouseover(function(){playSound("#click1-hover");});
+		// Load page
+		loadPage(location.hash);
+		window.onhashchange = function(){loadPage(location.hash);};
+		// Cactus
+		positionCactus();
+		$(window).resize(positionCactus);
+	}, function(){
+		$("body").html("error building project menu")
+	});
 }
 
 /*------------------------------Cactus Functions------------------------------*/
@@ -119,39 +130,98 @@ function unmute() {
 	$("#mute-btn").removeClass("muted");
 }
 
-/*---------------------------------Navigation---------------------------------*/
+/*----------------------------Content & Navigation----------------------------*/
 
-function loadSubpage(locHash) {
+function buildProjectMenu(successCallback, failureCallback) {
+	$.getJSON(projectListPath, function(result){
+		for (let i = 0; i < result.projects.length; i++) {
+			// Update project IDs list
+			projectIDs.push(result.projects[i].id);
+			// Build menu
+			const project = $("<a>")
+				.attr("href", "#project-" + result.projects[i].id)
+				.addClass("click1")
+				.text(result.projects[i].title);
+			$("#projects").append(project);
+		}
+		successCallback();
+	}).fail(failureCallback);
+}
+
+function loadPage(locHash) {
 	$("#content-box").empty();
 	// About me page
 	if (locHash == "#aboutme") {
-		$("#content-box").load("pages/aboutme.html", positionCactus);
 		$(".welcome").hide();
 		$(".subpage").show();
+		$(".close-btn").show();
+		$(".back-btn").hide();
+		$("#projects").hide();
+		$("#content-box").load("pages/aboutme.html", positionCactus);
 	// Projects page
 	} else if (locHash == "#projects") {
-		$.get("projects/test/test.md", function(data){
-			$("#content-box").html(marked.parse(data));
-			setTimeout(positionCactus, 0);
-		});
 		$(".welcome").hide();
 		$(".subpage").show();
+		$(".close-btn").show();
+		$(".back-btn").hide();
+		$("#projects").show();
+		$("#content-box").load("pages/projects.html", positionCactus);
 	// Links page
 	} else if (locHash == "#links") {
-		$("#content-box").load("pages/links.html", positionCactus);
 		$(".welcome").hide();
 		$(".subpage").show();
+		$(".close-btn").show();
+		$(".back-btn").hide();
+		$("#projects").hide();
+		$("#content-box").load("pages/links.html", positionCactus);
 	// Contact page
 	} else if (locHash == "#contact") {
-		$("#content-box").load("pages/contact.html", positionCactus);
 		$(".welcome").hide();
 		$(".subpage").show();
-	// Default to welcome page
+		$(".close-btn").show();
+		$(".back-btn").hide();
+		$("#projects").hide();
+		$("#content-box").load("pages/contact.html", positionCactus);
 	} else {
-		$("#content-box").load("pages/welcome.html", positionCactus);
-		$(".welcome").show();
-		$(".subpage").hide();
+		// Load project
+		const projectID = locHash.replace("#project-", "");
+		if (projectIDs.includes(projectID)) {
+			$(".back-btn").attr("href", "#projects");
+			$(".welcome").hide();
+			$(".subpage").show();
+			$(".close-btn").show();
+			$(".back-btn").show();
+			$("#projects").hide();
+			loadProject(projectID,
+				function(){setTimeout(positionCactus, 0);},
+				function(){
+					$("#content-box").text(`error loading ${projectID}.md`);
+					positionCactus();
+				});
+		// Default to welcome page
+		} else {
+			$(".welcome").show();
+			$(".subpage").hide();
+			$(".close-btn").hide();
+			$(".back-btn").hide();
+			$("#projects").hide();
+			$("#content-box").load("pages/welcome.html", positionCactus);
+		}
 	}
+}
+
+function loadProject(projectID, successCallback, failureCallback) {
+	const projectPath = projectDir + projectID + "/";
+	$.get(projectPath + projectID + ".md", function(data){
+		// Load markdown data
+		$("#content-box").html(marked.parse(data));
+		// Correct image source paths
+		$("#content-box img").each(function(){
+			const oldSrc = $(this).attr("src");
+			$(this).attr("src", projectPath + oldSrc);
+		});
+		successCallback();
+	}).fail(failureCallback);
 }
 
 /*--------------------------------More Actions--------------------------------*/
